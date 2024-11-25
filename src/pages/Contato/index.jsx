f59c97b9a7aa5b82c801e3c9c33ff9f895d49b00
Ivan Martins <160087356+IvanM4rtin5@users.noreply.github.com>
2024-11-25 17:33:41 -0300

    Update index.jsx

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -48,7 +48,7 @@ export function Contatos() {
                     
                     </a>
                     {/* Botão para baixar o currículo */}
-                    <a href="https://portfolio-prime.netlify.app/documents/NovoCurriculo.pdf" download="ivan_NovoCurriculo.pdf">
+                    <a href="https://docs.google.com/document/d/1woauvpt1LXX5a_HaGiYvkxTil3v_uGMjFnYThEGrKxU/export?format=pdf" download="Cv-Ivan.pdf" target="_blank">
                         <Button title="Baixar Currículo"/>
                     </a>
                 </ContactInfo>
